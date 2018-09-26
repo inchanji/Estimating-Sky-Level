dafdb8814d559e826e1c74c1b6c847f5ecd13963
Inchan Ji <iji@ucdavis.edu>
2018-09-26 18:09:56 +0900

    Add files via upload

---
+++ miscellanea/galsim/rungalsim_1img_repeat_Bndry.sh
@@ -0,0 +1,19 @@
+txt='.txt'
+fits='.fits'
+NCPU=32
+filename='rand_dlst_dls_depth_m27to29_2000by2000pix'
+ext='_set'
+randum='/home/inchani/galsim/randnum.txt'  # make your own a list of random numbers
+try=1
+while [ $try -lt 21 ];do
+num=$(sed -n $try"p" $randum)
+echo Try Num $try
+echo random number : $num 
+cp makegalaxies_in_one0Bndry.yaml makegalaxies_in_one.yaml 
+sed -i -e "31s/RADOMNUMBER/$num/g" makegalaxies_in_one.yaml
+sed -i -e "32s/NCPU/$NCPU/g" makegalaxies_in_one.yaml 
+sed -i -e "42s/FILENAME/$filename$txt/g" makegalaxies_in_one.yaml 
+sed -i -e "50s/FILENAME/$filename$ext$try$fits/g" makegalaxies_in_one.yaml 
+galsim makegalaxies_in_one.yaml 
+try=$(($try+1))
+done
